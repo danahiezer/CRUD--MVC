73d51dcd64174ac449a066ef2750ce848c5b6c57
danahiezer <dansantacruz5@gmail.com>
2026-04-13 19:52:26 -0300

    correccion de controlador para get('/crear')

--- routes/temaRoutes.js
+++ routes/temaRoutes.js
@@ -4,13 +4,14 @@ const router = express.Router()
 const {
     listarTemas,
     mostrarEditorForm,
+    mostrarFormulario,
     crearTemacontroller,
     actualizarTemacontroller,
     eliminarTemaController
 } = require('../controllers/temaController')
 
 router.get('/', listarTemas) // ver lista
-router.get('/crear', mostrarEditorForm) // ver form para crear
+router.get('/crear', mostrarFormulario) // ver form para crear
 router.post('/crear', crearTemacontroller)// enviar form crear
 router.get('/:id/editar', mostrarEditorForm)// ver form editar 
 router.post('/:id/editar', actualizarTemacontroller)// enviar form editar
