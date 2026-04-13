a8cb6adbdeff9e2156bf835b49a3d28014ff528f
danahiezer <dansantacruz5@gmail.com>
2026-04-13 18:23:51 -0300

    rutas del CRUD para determinar que controlador (acciones) deben ejecutarse

---
+++ routes/temaRoutes.js
@@ -0,0 +1,19 @@
+// responsabilidad: deifinir que controlador se llama segun la URL
+const express = require('express')
+const router = express.Router()
+const {
+    listarTemas,
+    mostrarEditorForm,
+    crearTemacontroller,
+    actualizarTemacontroller,
+    eliminarTemaController
+} = require('../controllers/temaController')
+
+router.get('/', listarTemas) // ver lista
+router.get('/crear', mostrarEditorForm) // ver form para crear
+router.post('/crear', crearTemacontroller)// enviar form crear
+router.get('/:id/editar', mostrarEditorForm)// ver form editar 
+router.post('/:id/editar', actualizarTemacontroller)// enviar form editar
+router.post('/:id/elimnar', eliminarTemaController)// eliminar tema
+
+module.exports = router
