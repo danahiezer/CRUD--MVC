7f3864617b8e45c01c4b4548b499e8027254acd9
danahiezer <dansantacruz5@gmail.com>
2026-04-14 20:08:07 -0300

    cambio total en temaModel para poder utilizar MongoDB

--- models/temaModel.js
+++ models/temaModel.js
@@ -1,30 +1,13 @@
-// responsabilidad: manejar los datos de los temas 
-let temas = [
-    { id: 1, titulo: "como programar como un ninja", votos: 0 },
-    {id: 2, titulo: "dominar el arter de preparar cafe", votos: 0}
-]
+// models/temaModel.js
+const mongoose = require('mongoose')
 
-let nextId = 3
+// Definir la estructura del documento
+const temaSchema = new mongoose.Schema({
+  titulo: { type: String, required: true },
+  votos: { type: Number, default: 0 }
+})
 
-// obtener datos de los temas
-const obtenerTemas = () => temas
+// Crear el modelo
+const Tema = mongoose.model('Tema', temaSchema)
 
-// crear un tema nuevo 
-const crearTema = (titulo) => {
-    const nuevoTema = { id: nextId++, titulo, votos: 0 }
-    temas.push(nuevoTema)
-    return nuevoTema
-}
-
-// actualizar un tema
-const actualizarTema = (id, titulo) => {
-    const tema = temas.find(t => t.id === parseInt(id)) // busca el primer elemento que cumpla una condicion
-    if (tema) tema.titulo = titulo
-    return tema
-}
-
-// eliminar un tema
-const eliminarTema = (id) => {
-    temas = temas.filter(t => t.id !== parseInt(id)) // devuelve un nuevo array sin los elemntos que no cumplan una condicion
-}
-module.exports = { obtenerTemas, crearTema, actualizarTema, eliminarTema }
+module.exports = Tema
