0a042b5a44a2509d72b44f07e63d186f895f758a
danahiezer <dansantacruz5@gmail.com>
2026-04-16 17:36:10 -0300

    agregamos linea de express.static

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const app = express()
 const PORT = 3000
 
 // Conectar a MongoDB
-mongoose.connect('mongodb://192.168.100.22:27017/learn_it_love_it')
+mongoose.connect('mongodb://192.168.21.143:27017/learn_it_love_it')
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.log('Error conectando a MongoDB:', err))
 
 // Motor de plantillas
 app.set('view engine', 'ejs')
+
+// express sirve los archivos de la carpeta public directo al navegador 
+app.use(express.static('public'))
 
 // dice a express que lea datos del form html y ponga en req.body o si no estaria vacio siempre
 app.use(express.urlencoded({ extended: true}))
@@ -23,5 +26,5 @@ app.use('/temas', temaRoutes)
 
 // Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+  console.log(`Servidor corriendo en http://localhost:${PORT}/temas`)
 })
