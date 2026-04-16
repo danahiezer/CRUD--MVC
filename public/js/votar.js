abec005eca36abc13439e7acec2daa59ebf71090
danahiezer <dansantacruz5@gmail.com>
2026-04-16 18:24:56 -0300

    codigo para reordenar votos de mayor a menor sin refrescar el navegador

--- public/js/votar.js
+++ public/js/votar.js
@@ -10,8 +10,21 @@ document.querySelectorAll('.btn-votar').forEach(boton => { // selecciona un boto
 
         const datos = await respuesta.json() // espera que se cumpla la promesa y kuego devuelve la respuesta en json
 
+        // actualizar el contador
         const li = document.getElementById(`tema-${id}`)
         li.querySelector('.contador-votos').textContent = datos.votos // actualiza nro de votos sin recargar pagina
+
+        // reordenar la lista de temas
+        const ul = li.parentElement
+        const items = Array.from(ul.querySelectorAll('li')) // convierte los li de la lista en arrays para ordenarlos
+
+        items.sort((a, b) => { // sort ordena el array de mayor a menor en votos
+            const votosA = parseInt(a.querySelector('.contador-votos').textContent)
+            const votosB = parseInt(b.querySelector('.contador-votos').textContent)
+            return votosB - votosA
+        })
+
+        items.forEach(item => ul.appendChild(item)) // inserta li en un nuevo orden y el navegador los mueve automaticamente sin recargar 
 
     })
 })
