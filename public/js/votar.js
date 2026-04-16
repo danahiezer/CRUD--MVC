a5d91e0a68cd7101e3c5d4094c371e7f86604145
danahiezer <dansantacruz5@gmail.com>
2026-04-16 18:10:50 -0300

    comentarios

--- public/js/votar.js
+++ public/js/votar.js
@@ -1,17 +1,17 @@
 // public/js/votar.ja
 
-document.querySelectorAll('.btn-votar').forEach(boton => {
-    boton.addEventListener('click', async (e) => {
-        const id = e.target.dataset.id
+document.querySelectorAll('.btn-votar').forEach(boton => { // selecciona un boton de temas y ejecuta una accion
+    boton.addEventListener('click', async (e) => { // escucha el click de ese boton
+        const id = e.target.dataset.id // lee el atributo data-id de temas
 
-        const respuesta = await fetch(`/temas/${id}/votar`, {
+        const respuesta = await fetch(`/temas/${id}/votar`, { // envia peticion al server sin recargar pagina
             method: 'POST'
         })
 
-        const datos = await respuesta.json()
+        const datos = await respuesta.json() // espera que se cumpla la promesa y kuego devuelve la respuesta en json
 
         const li = document.getElementById(`tema-${id}`)
-        li.querySelector('.contador-votos').textContent = datos.votos
+        li.querySelector('.contador-votos').textContent = datos.votos // actualiza nro de votos sin recargar pagina
 
     })
 })
